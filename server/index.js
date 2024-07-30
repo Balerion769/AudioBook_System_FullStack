9b44031b59e366adf9f62c5a42ed21ea3d295d3f
Ravi Raj Meena <101081595+Balerion769@users.noreply.github.com>
2024-07-30 23:08:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import  reviewsRouter from './routes/reviews.js';
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000'  
+  origin: 'https://audio-book-system-full-stack.vercel.app'  
 }));
 app.use('/api/audiobooks', audiobooksRouter);
 app.use('/api/reviews', reviewsRouter);
